1f46afc66e41a9db568a33a0a864211c19eab466
Josha Bartsch <jaybraker1@gmail.com>
2021-10-22 17:30:56 +0200

    Testklasse nach Anforderung, ohne Ressourcen und Pfade

---
+++ Test.java
@@ -0,0 +1,19 @@
+package abgabe3;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        try {
+            EMailAdressBuch extern = new EMailAdressBuch();
+            EMailAdressBuch intern = new EMailAdressBuch();
+        
+            extern.einlesen("mitarbeiter_matse_extern.txt");
+            intern.einlesen("mitarbeiter_matse_intern.txt");
+            System.out.println(extern.toString());
+            System.out.println(intern.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
