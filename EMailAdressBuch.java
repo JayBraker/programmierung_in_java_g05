363b4c9fb07d23aa5a763193e54087ab8e82201f
JayBraker <jaybraker1@gmail.com>
2021-10-25 10:25:05 +0200

    Martins Version

--- EMailAdressBuch.java
+++ EMailAdressBuch.java
@@ -1,109 +1,461 @@
+
+[
+14:04
+]
+Martin.A
+:
+eghhh joahr
+[
+14:04
+]
+Martin.A
+:
+sollte mal anfangen
+[
+17:40
+]
+Martin.A
+:
+Hey, 
+@Selina Fiorin , @JayBraker  habt ihr morgen abend so um 20 uhr zeit, damit wir über die Java.Ha sprechen können, wer diesmal abgibt und wo es probleme gibt. Danke 🙂
+[
+17:40
+]
+JayBraker
+:
+Ich hoffe ich bin dann da, kann maximal 21 Uhr bei mir werden denke ich
+[
+17:41
+]
+Martin.A
+:
+soll nur ein kurzes, wie weit ist wer, und gibt es methoden, wo man nicht weis wie das klappen soll. mehr nicht, dauert keine std max 30 min 
+[
+17:43
+]
+JayBraker
+:
+Ja klar, das geht für mich
+[
+17:05
+]
+JayBraker
+:
+Also ich bin wider erwarten ab jetzt, bis beliebig spät online. Könnte mich heute ganz nach euch richten, ab morgen wirds eher schwierig für mich.
+Klappt es heute abend @Selina Fiorin @Golem013 ?
+[
+17:09
+]
+Golem013
+:
+klappt bei mir
+[
+20:16
+]
+JayBraker
+:
+Wäre in 10 Minuten mit Abendessen da zur Besprechung. Passt das?
+[
+20:16
+]
+Martin.A
+:
+ehhh joahr
+JayBraker
+ hat einen Anruf gestartet, der 38 Minuten gedauert hat.
+ — 
+gestern um 20:24 Uhr
+[
+20:37
+]
+Martin.A
+:
+private boolean nameIstVorhanden(String name)
+    {
+        if(h.containsKey(name))
+        {return true;}
+        return false;
+    }
+[
+09:02
+]
+Martin.A
+:
+ich bin soweit fertig mit dem Code, doc kommentare und normale Kommentare auch schon vorhanden 
+[
+09:04
+]
+Martin.A
+:
+@JayBraker als info, du hast bei der methode einlesen im JavaDoc kommentar ein @throws gemacht. 
+Logisch gesehen nicht falsch aber ds kannst du nur machen wenn die methode so aussieht
+[
+09:04
+]
+Martin.A
+:
+die Methode braucht ein "throws"
+[
+09:06
+]
+Martin.A
+:
+Deswegen habe ich das @throws entfernt, da laut aufgaben vorgabe die Methode kein throws haben soll.
+Stattdessen werde ich das als Bemerkung in den Javadoc Kommentar schreiben 
+[
+09:28
+]
+Selina Fiorin
+:
+es tut mir leid, ich konnte gestern nicht dabei sein. kann mir jemand sagen, was ihr gestern besprochen habt?
+[
+09:31
+]
+Martin.A
+:
+wir haben darüber gesprochen, wie weit wir sind und was noch fehlt. Ich habe jetzt alles kommentiert, Javadocs hinzugefügt und etwas "verfeinert" (nur den Satzbau korrigiert) und lade es jetzt mal aufs Git hub hoch
+[
+09:35
+]
+Martin.A
+:
+Josha, du musst mir mal berrechtigungen geben, dmait ich einen branch machen kann, der will das immer direkt mergen, was es aber nit soll
+[
+09:40
+]
+Selina Fiorin
+:
+ehrlich gesagt, hab ich noch nicht damit angfangen, hatte einen stressvollen weekend, ich hoffe ihr nehmt es mir nicht übel. ich mach mich gleich an die Arbeit, gibt es auf GitHub noch etwas was gemacht werden soll?
+[
+09:41
+]
+Selina Fiorin
+:
+oder was ich machen könnte
+[
+09:41
+]
+Martin.A
+:
+nope
+[
+09:41
+]
+Martin.A
+:
+sind komplett fertig
+[
+09:42
+]
+Martin.A
+:
+mein code hab ich nochnicht auf github hochgeladen, da er mich kein neuen branch anlegen lässt sondern direkt die verbinden möchte 
+[
+09:42
+]
+Selina Fiorin
+:
+Können wir es nicht das nächste mal aufteilen?
+[
+09:42
+]
+JayBraker
+:
+Habe dich gerade als Collab eingeladen
+[
+09:45
+]
+Martin.A
+:
+kannst du mir erklären, warum der direkt die anderen dateien also deine uploads immer mit reinpackt? 
+[
+10:00
+]
+Selina Fiorin
+:
+also, ich verstehe wenn ihr wegen letzter Woche angepisst seid, aber ich denke wir sollten trotzdem als Team arbeiten und nicht gegeneinander. Es ist super wenn ihr euch beiden versteht, aber ihr seit nicht alleine in der Gruppe. Mir ist klar, dass ich als anfänger nicht viel beitragen kann und es euch vielleicht stört mit einer Frau in der Gruppe zu sein. Es ist nun mal so, und wir sollten versuchen einanderzuhelfen und voneinander zu lernen. ich kann mich bereit erklären für nächste Woche ein Task-Plan herzustellen, kann dann jeder selbst wählen welcher Aufgaben ihn besser passen und anschliessend in Git uploaden. Wäre das eine Idee?
+[
+10:07
+]
+Martin.A
+:
+Also ich glaube du interpretierst da Zuviel rein. 
+Das letzte Woche, passiert halt, kann man nichts machen und es ist dir keiner sauer deswegen.
+Nur war das jetzt so, dass du gestern halt nicht da warst und anstatt dann nichts zu machen, haben wir einfach geschaut, wie weit wir sind
+ und festgestellt, das wir schon fertig sind.
+Die Aufgabe war nicht schwer, deswegen sind wir fertig und nicht weil wir irgendwie nur zu zweit zusammenarbeiten 
+[
+10:09
+]
+Martin.A
+:
+Am besten ist, dass wir einen festen tag ausmachen, an dem wir uns immer zusammensetzen und schauen wie weit wir sind.
+Ich würde dafür sonntags ab 19 uhr vorschlagen.
+[
+10:10
+]
+Selina Fiorin
+:
+ich wäre für 1800
+[
+10:10
+]
+Martin.A
+:
+mir recht
+[
+10:13
+]
+JayBraker
+:
+Mir auch
+[
+10:13
+]
+JayBraker
+:
+Ich verstehe die Frage nicht
+[
+10:13
+]
+JayBraker
+:
+Wann macht er was?
+[
+10:14
+]
+Martin.A
+:
+wenn ich eine datei anlege, dann macht der ein branch, wo auch deine files drin sind, also die testklasse und die Exception klasse obwohl ich nur 1 datei mit meinem code hochgeladen habe 
+[
+10:15
+]
+JayBraker
+:
+Öhm
+[
+10:16
+]
+JayBraker
+:
+Du hast ja deinen Branch, ich würde an deiner Stelle in Eclipse das Repository initialisieren und dann dass was du bisher hast direkt in deinen Branch pushen
+[
+10:17
+]
+Martin.A
+:
+ach, ich soll das github in eclipse einbinden 
+[
+10:17
+]
+Martin.A
+:
+uff ne du
+[
+10:17
+]
+Martin.A
+:
+kein plan wie das geht
+[
+10:18
+]
+Martin.A
+:
+also ich sollte ein github.. irgendwas von meiner firma importieren und einbinden, ne danke mach ich nicht nochmal
+[
+10:20
+]
+JayBraker
+:
+Also
+
+git init
+git add .
+git commit -m "Initial import"
+git remote add origin https://git@github.com/JayBraker/programmierung_in_java_03.git
+git push -u origin Finale-version
+
+[
+10:20
+]
+JayBraker
+:
+Geht nur wenn du einen SSH Schlüssel in Github hinterlegt hast
+[
+10:21
+]
+Martin.A
+:
+ahja dass hab ich ganz bestimmt.........nicht🙃
+[
+10:22
+]
+JayBraker
+:
+Das macht er immer sobald du einen Branch erstellst - das ist ja kein eigenes Projekt sondern nur eine Abspaltung des Master-Branch.
+Du kannst die Dateien auch im Browser editieren und die Inhalte ersetzen durch deinen eigenen Code
+[
+10:23
+]
+JayBraker
+:
+Kann ich dir auch nicht helfen, ich weiss nicht was deine Firma für Git nutzt
+[
+10:23
+]
+Martin.A
+:
+ja kannste mir ja später mal erklären
+[
+10:24
+]
+Martin.A
+:
+nur jetzt greade kein plan und ich werds einfach als txt hier rein schicken
+[
+10:24
+]
+Martin.A
+:
+
+package Java_HA3;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+Mehr anzeigen
+message.txt5 kB
+﻿
+
+package Java_HA3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import Java_HA02.Goldtagespreis;
 
 /**
  * @author Martin Albertz, Selina Fiorin, Josha Bartsch
  *
  */
-
 public class EMailAdressBuch
 {
-    private final HashMap<String, String> userList;
+	private HashMap<String, String> emailListe;
+	private Scanner sc;
 
-    /**
-     * 
-     * Basic constructor initializes the HashMap.
-     * 
-     */
-    public EMailAdressBuch()
-    {
-        this.userList = new HashMap<>();
-    }
+	/**
+	 * Basic constructor initializes the HashMap.
+	 */
+	public EMailAdressBuch()
+	{
+		emailListe = new HashMap<>();				//legt eine neue hasMap An
+	}
 
-    /**
-     * Copy-constructor copies the originals HashMap storing names and addresses.
-     * 
-     * @param the original object to be copied
-     */
-    public EMailAdressBuch(EMailAdressBuch e)
-    {
-        this.userList = e.userList;
-    }
+	/**
+	 * Copy-constructor copies the originals HashMap storing names and addresses.
+	 * @param e the EmailAdressBuch object to be copied
+	 */
+	public EMailAdressBuch(EMailAdressBuch e)
+	{
+		this.emailListe = e.emailListe;
+	}
 
-    /**
-     * Method einfuegen enters a supplied name, address tuple into the HashMap.
-     * 
-     * @param name supplied name String.
-     * @param email supplied address String.
-     */
-    public void einfuegen(String name, String email)
-    {
-        this.userList.put(name, email);
-    }
+	/**
+	 * Method einfuegen adds the supplied name, address tuple into the HashMap.
+	 * if there is already an entry with the same name, it is simply overwritten and only the email is updated.
+	 * @param name supplied name String.
+	 * @param email supplied address String.
+	 */
+	public void einfuegen(String name, String email)
+	{
+		if (nameIstVorhanden(name))					// Wenn name vorhanden,
+		{
+			emailListe.put(name, email); 			// ueberschreibe den eintrag, indem ein gleicher angelegt wird
+		} else
+		{
+			emailListe.put(name, email); 			// Wenn name nicht vorhanden, dann leg an
+		}
+	}
 
-    /**
-     * Method abfrage returns the address associated to a given name String. Throws
-     * a custom exception if the name is not stored in the HashMap.
-     * 
-     * @param name supplied name String.
-     * @return address String.
-     */
-    public String abfrage(String name)
-    {
-        if (!this.userList.containsKey(name))
-        {
-            throw new UnknownNameException(
-                    String.format("Kein Eintrag auf den Namen %s", name));
-        }
-        return this.userList.get(name);
-    }
+	/**
+	 * Checks if the supplied entry is already in the list, if so, it will return "true" otherwise it will return "false
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private boolean nameIstVorhanden(String name)
+	{
+		if (emailListe.containsKey(name))			// wenn name schon vorhanden ist
+		{return true;}								// gib "true" zurueck
+		return false;								// falls nicht, gib "false" zurueck
+	}
 
-    /**
-     * Custom toString Method joins every key-value entry in the HashMap to the
-     * following format: {name1=address1, name2=address2, ...}
-     */
-    public String toString()
-    {
-        StringBuilder builder = new StringBuilder("");
-        builder.append("{");
-        if (!userList.isEmpty())
-        {
-            for (String s : userList.keySet())
-            {
-                builder.append(s);
-                builder.append("=");
-                builder.append(userList.get(s));
-                builder.append(", ");
-            }
-            builder.delete(builder.length() - 2, builder.length());
-        }
-        builder.append("}");
-        return builder.toString();
-    }
+	/**
+	 * Method abfrage returns the address associated to a given name String.
+	 * Throws a custom exception if the name is not stored in the HashMap.
+	 * 
+	 * @param name supplied name String.
+	 * @return the email associated with the given name as a String.
+	 */
+	public String abfrage(String name)
+	{
+		if (!nameIstVorhanden(name)) 				// wenn nicht vorhanden, throw exception
+		{
+			throw new UnknownNameException(String.format("Kein Eintrag auf den Namen %s", name));
+		} else
+		{return emailListe.get(name);}				// wenn Vorhanden, liefert die E-Mail-Adresse zum gegebenen Namen 
+	}
 
-    /**
-     * Method einlesen attempts to read a specified textfile. Expects one
-     * name;address tuple per line.
-     * 
-     * @param dateiname supplied String must be absolute Path to file.
-     * @throws FileNotFoundException if supplied String was not valid.
-     */
-    public void einlesen(String dateiname) throws FileNotFoundException
-    {
-        File source = new File(dateiname);
-        Scanner sc = new Scanner(source);
+	/**
+	 * Custom toString Method joins every key-value entry in the HashMap to the
+	 * following format: {name1=address1, name2=address2, ...}
+	 * @return a String with all the entries in the mentioned Format
+	 */
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder("");
+		builder.append("{");
+		if (!emailListe.isEmpty())					// wenn nicht leer
+		{
+			for (String s : emailListe.keySet())	// fuer jeden eintrag mache
+			{
+				builder.append(s);					// hole den namen der person
+				builder.append("=");				// haenge ein "=" dran
+				builder.append(emailListe.get(s));	// haenge die email dran
+				builder.append(", ");				// komma und leerzeichen
+			}
+			builder.delete(builder.length() - 2, builder.length()); // Leerzeichen und komma wird entfernt, wenn am ende.
+		}
+		builder.append("}");						// "}" dranhaengen
+		return builder.toString();					// ausgeben
+	}
 
-        while (sc.hasNextLine())
-        {
-            String line = sc.nextLine();
-            if (!line.isBlank())
-            {
-                String[] entry = line.split(";");
-                this.einfuegen(entry[0], entry[1]);
-            }
-        }
-        sc.close();
-    }
+	/**
+	 * Method einlesen attempts to read a specified textfile. 
+	 * Expects one name;address tuple per line.
+	 * Results in a FileNotFoundException if the supplied String was not valid.
+	 * 
+	 * @param dateiname supplied String must be an absolute Path to file.
+	 */
+	public void einlesen(String dateiname)
+	{
+		try
+		{
+			sc = new Scanner(new File(dateiname));	// scanner auf datei setzen
+			while (sc.hasNextLine() == true)		// solange es noch eine naechste zeile gibt
+			{
+				String s = sc.nextLine();			// hole die naechste zeile
+				String[] sa = s.split(";");			// Teile den String um ";" auf
+				emailListe.put(sa[0], sa[1]); 		// mappe die sachen (sa[0] = name, sa[1]= email)
+			}
+			sc.close();								// wenn fertig, schließe scanner
+		} catch (FileNotFoundException e)			// Der Fehler, der geworfen wird, wenn keine datei vorhanden ist
+		{e.printStackTrace();}
+	}
 }
