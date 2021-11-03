95bac069c9dc015196989c7a9a165e0a078a01a5
Golem013 <93118651+Golem013@users.noreply.github.com>
2021-11-03 18:47:20 +0100

    Update Rechenoperationsliste.java

--- Rechenoperationsliste.java
+++ Rechenoperationsliste.java
@@ -18,7 +18,7 @@ public class Rechenoperationsliste
      */
     public Rechenoperationsliste()
     {
-        this.ops = new ArrayList<Rechenoperation>();
+        this.ops = new ArrayList<Rechenoperation>();    //setzte die Arraylist
     }
 
     /**
@@ -28,36 +28,36 @@ public class Rechenoperationsliste
      */
     public void add(Rechenoperation operation)
     {
-        ops.add(operation);
+        ops.add(operation);         // ruft die .add funktion für ops auf
     }
     
     /** 
      * @return Returns the ArrayList.
      */
     public ArrayList<Rechenoperation> getList(){
-        return this.ops;
+        return this.ops;            // gibt dises ops objekt aus
     }
 
     /**
      * Calling this method implies that at least one Rechenoperation was added beforehand.
-     * his method applies every single operation that was added to every element of the supplied Array. 
+     * this method applies every single operation that was added to every element of the supplied Array. 
      * 
      * @param feld Array containing any number of double values.
      * @return Array of the same size as feld, but every element was is transformed according to the Rechenoperationen added.
      */
     public double[] transform(double[] feld)
     {
-        double[] ret = feld.clone();
+        double[] ret = feld.clone();                // klont das feld nach ret
 
-        for (Rechenoperation operation : ops)
+        for (Rechenoperation operation : ops)       // fuer jedes element
         {
-            for (int i = 0; i < feld.length; i++)
+            for (int i = 0; i < feld.length; i++)       //solange, wie i < feldgroesse
             {
-                double x = ret[i];
-                x = operation.berechne(x);
-                ret[i] = x;
+                double x = ret[i];                  // holt sich den wert an stelle i und speichert diesen in x
+                x = operation.berechne(x);          // fuehrt die methode berrechne mit dem x aus und speichert das ergebniss in das x
+                ret[i] = x;                         // an stelle i, wird nun das der wert mit x ueberschriben
             }
         }
-        return ret;
+        return ret;                                 // gibt das ret feld aus
     }
 }
