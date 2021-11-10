e05ea3724661bdec3fc6608ca2a352f7b0f19354
JayBraker <jaybraker1@gmail.com>
2021-11-10 15:59:56 +0100

    Add Javadoc comments

--- Pavlov.java
+++ Pavlov.java
@@ -1,29 +1,32 @@
 package abgabe5;
 
+/**
+ * This method implements the "Pavlov" strategy.
+ * It imperates that the player will cooperate if his action in the last turn matched the action of his opponent.
+ * Otherwise if their actions differed, he will betray in the current turn.
+ * 
+ * @author Selina Fiorin, Martin Albertz, Josha Bartsch
+ */
 public class Pavlov implements GefangenenStrategie
 {
+    /*
+     * To compare his own actions to those of his opponent,
+     * the pavlovian player must remember his last action in addition to his opponents last action.
+     */
     private boolean oppLastDec;
     private boolean ownLastDec;
 
     /**
-     * 
-     */
-    public Pavlov()
-    {
-        this.oppLastDec = true;
-        this.ownLastDec = true;
-    }
-    
-    /**
+     * Pavlovs decisionmaking compares the last performed actions of both players,
+     * if these match, he will cooperate, otherwise he will betray.
+     * He also must save this decision for the next turn.
      *
+     * @return boolean The decision based upon Pavlov strategy.
      */
     @Override
     public boolean getNextDecision()
     {
         boolean decision;
-        /*
-         * 
-         */
         if (this.oppLastDec == this.ownLastDec) {
             decision = true;
         } else {
@@ -35,7 +38,7 @@ public class Pavlov implements GefangenenStrategie
     }
 
     /**
-     *
+     * Receive information on the opponents action during the last turn.
      */
     @Override
     public void setOpponentsLastDecision(boolean decision)
