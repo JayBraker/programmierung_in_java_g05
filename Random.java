7c46185a2fa7de221f213db6cf4cb648e97f8123
Golem013 <93118651+Golem013@users.noreply.github.com>
2021-11-10 19:07:08 +0100

    Create Random.java

--- Random.java
+++ Random.java
@@ -10,27 +10,27 @@ public class Random implements GefangenenStrategie
      */
     public Random()
     {
-        this.rand = new java.util.Random();
+        this.rand = new java.util.Random();     // generiert zufaellig einen true oder false wert
         this.oppLastDec = true;
     }
-
+    
     /**
-     *
+     * This methode gets the new decision of a player
+     * @return boolean if he should cooperate (true) or not (false) 
      */
     @Override
     public boolean getNextDecision()
     {
-        return rand.nextBoolean();
+        return rand.nextBoolean();          // generiert zufaellig einen neuen true oder false wert für die naechste entscheidung
     }
 
     /**
-     *
+     *@param boolean the decision of the opponent
      */
     @Override
     public void setOpponentsLastDecision(boolean decision)
     {
-        this.oppLastDec = decision;
-
+        this.oppLastDec = decision;          // speichert die entscheidung des gegners
     }
 
 }
