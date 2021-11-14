e7f07e771f1bc7d124f759305052f402e5be221d
Josha Bartsch <jaybraker1@gmail.com>
2021-11-14 16:11:20 +0100

    Reformatted file

--- BlattVergleich.java
+++ BlattVergleich.java
@@ -8,40 +8,60 @@ public class BlattVergleich implements Comparator<Blatt>
     @Override
     public int compare(Blatt o1, Blatt o2)
     {
-        if(o1.isDrilling() && o2.isDrilling()) {
-            if(o1.getDrillingValue() < o2.getDrillingValue()) {
+        if (o1.isDrilling() && o2.isDrilling())
+        {
+            if (o1.getDrillingValue() < o2.getDrillingValue())
+            {
                 return -1;
-            } else if (o1.getDrillingValue() == o2.getDrillingValue()) {
+            } else if (o1.getDrillingValue() == o2.getDrillingValue())
+            {
                 return 0;
-            } else {
+            } else
+            {
                 return 1;
             }
-        } else if (o1.isZwilling() && o2.isZwilling()) {
-            if(o1.getZwillingValue() < o2.getZwillingValue()) {
+        } else if (o1.isZwilling() && o2.isZwilling())
+        {
+            if (o1.getZwillingValue() < o2.getZwillingValue())
+            {
                 return -1;
-            } else if (o1.getZwillingValue() > o2.getZwillingValue()) {
+            } else if (o1.getZwillingValue() > o2.getZwillingValue())
+            {
                 return 1;
-            } else if (o1.getThirdCard() < o2.getThirdCard()) {
+            } else if (o1.getThirdCard() < o2.getThirdCard())
+            {
                 return -1;
-            } else if (o1.getThirdCard() > o2.getThirdCard()) {
+            } else if (o1.getThirdCard() > o2.getThirdCard())
+            {
                 return 1;
-            } else {
+            } else
+            {
                 return 0;
             }
-        } else if (o1.isDrilling() && o2.isZwilling()) {
-            return 1;            
-        } else if (o1.isZwilling() && o2.isDrilling()) {
+        } else if (o1.isDrilling() && o2.isZwilling())
+        {
+            return 1;
+        } else if (o1.isZwilling() && o2.isDrilling())
+        {
             return -1;
-        } else if ((o1.isZwilling() || o1.isDrilling()) && !(o2.isZwilling() || o2.isDrilling())) {
+        } else if ((o1.isZwilling() || o1.isDrilling())
+                && !(o2.isZwilling() || o2.isDrilling()))
+        {
             return 1;
-        } else if (!(o1.isZwilling() || o1.isDrilling()) && (o2.isZwilling() || o2.isDrilling())) {
+        } else if (!(o1.isZwilling() || o1.isDrilling())
+                && (o2.isZwilling() || o2.isDrilling()))
+        {
             return 1;
-        } else {
-            if (o1.getSum() < o2.getSum()) {
+        } else
+        {
+            if (o1.getSum() < o2.getSum())
+            {
                 return -1;
-            } else if (o1.getSum() > o2.getSum()) {
+            } else if (o1.getSum() > o2.getSum())
+            {
                 return 1;
-            } else {
+            } else
+            {
                 return 0;
             }
         }
