8285fff3f9448c78695490cb9e13d7cc944d6c4b
Josha Bartsch <jaybraker1@gmail.com>
2021-11-21 16:12:26 +0100

    Implemented Loesungsalgorithmus, loop random ints until [0,0] == 1

---
+++ SchiebeAlg1.java
@@ -0,0 +1,29 @@
+package abgabe7;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SchiebeAlg1 implements Loesungsalgorithmus {
+
+  Schiebepuzzle puzzle;
+
+  @Override
+  public void loese(Schiebepuzzle sp) {
+    this.puzzle = sp;
+
+    int[] posTarget = {0, 0};
+
+    int c = 0;
+    int cs = 0;
+    while (!(sp.getVal(0, 0) == 1)) {
+      c++;
+      int pl = ThreadLocalRandom.current().nextInt(1, 15 + 1);
+      if (sp.istVerschiebbar(pl)) {
+        cs++;
+        sp.schiebe(pl);
+      }
+    }
+    System.out.println(String.format("Es hat %d Zufallszahlen gebraucht um zur Lösung zu kommen.", c));
+    System.out.println(String.format("Davon waren %d versuchte Zuege zulaessig!\n", cs));
+    return;
+  }
+}
