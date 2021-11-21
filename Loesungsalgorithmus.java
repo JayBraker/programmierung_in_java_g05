f5737f09f79108160bfdc8f9e9d4fe3454400cbc
Josha Bartsch <jaybraker1@gmail.com>
2021-11-21 17:49:43 +0100

    Added JavaDic

--- Loesungsalgorithmus.java
+++ Loesungsalgorithmus.java
@@ -1,5 +1,13 @@
 package abgabe7;
 
+/**
+ * Interface allows implementations of an algorith to solve Schiebepuzzles.
+ * 
+ * @author Martin Albertz, Selina Fiorin, Josha Bartsch
+ */
 public interface Loesungsalgorithmus {
+  /**
+   * @param sp Initalized Object of the class Schiebepuzzle to solve.
+   */
   void loese(Schiebepuzzle sp);
 }
