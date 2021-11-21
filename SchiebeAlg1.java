1bd8714001207e328f1c60dc7f86842fabdb0212
Josha Bartsch <jaybraker1@gmail.com>
2021-11-21 17:55:04 +0100

    Added JavaDocs to SchiebeAlg1

--- SchiebeAlg1.java
+++ SchiebeAlg1.java
@@ -2,18 +2,20 @@ package abgabe7;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * @author Martin Albertz, Selina Fiorin, Josha Bartsch
+ *
+ */
 public class SchiebeAlg1 implements Loesungsalgorithmus {
 
-  Schiebepuzzle puzzle;
-
+  /**
+   * Repeats random moves until the slot in row 1, column 1 has the value 1. The functionality is
+   * similar to Schiebepuzzle.mische() but it runs until the condition (feld[0][0]==1) is met.
+   */
   @Override
   public void loese(Schiebepuzzle sp) {
-    this.puzzle = sp;
-
-    int[] posTarget = {0, 0};
-
-    int c = 0;
-    int cs = 0;
+    int c = 0; // Counts every iteration, the iteration may be invalid if the chosen slot is not movable
+    int cs = 0; // Counts every performed move.
     while (!(sp.getVal(0, 0) == 1)) {
       c++;
       int pl = ThreadLocalRandom.current().nextInt(1, 15 + 1);
@@ -22,7 +24,8 @@ public class SchiebeAlg1 implements Loesungsalgorithmus {
         sp.schiebe(pl);
       }
     }
-    System.out.println(String.format("Es hat %d Zufallszahlen gebraucht um zur Lösung zu kommen.", c));
+    System.out
+        .println(String.format("Es hat %d Zufallszahlen gebraucht um zur Loesung zu kommen.", c));
     System.out.println(String.format("Davon waren %d versuchte Zuege zulaessig!\n", cs));
     return;
   }
