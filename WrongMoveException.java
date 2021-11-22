f6c73a670ee4675d4326903e91ecfdd01450db80
eunigalelunir <94292706+eunigalelunir@users.noreply.github.com>
2021-11-22 22:48:18 +0100

    Update WrongMoveException.java
    Punkt im Kommentar

--- WrongMoveException.java
+++ WrongMoveException.java
@@ -2,7 +2,7 @@ package abgabe7;
 
 /**
  * This Exception may be thrown in the context of an instance of Schiebepuzzle when a performed move
- * was not allowed
+ * was not allowed.
  *
  * @author Martin Albertz, Selina Fiorin, Josha Bartsch
  */
