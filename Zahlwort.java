d3a26c64163b5ba60c7aa87e0ae0c404c6780ba2
JayBraker <jaybraker1@gmail.com>
2021-12-15 13:46:37 +0100

    Finished the assignment

--- Zahlwort.java
+++ Zahlwort.java
@@ -1,98 +1,71 @@
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.stream.Stream; 
 
-public class Zahlwort {
+public class Zahlwort 
+{ 
+    private static HashMap<Integer, String> digits, digits2; 
+    static { 
+        digits = new HashMap<>(); 
+        digits.put(0, "null"); 
+        digits.put(1, "ein"); 
+        digits.put(2, "zwei"); 
+        digits.put(3, "drei"); 
+        digits.put(4, "vier"); 
+        digits.put(5, "fünf"); 
+        digits.put(6, "sechs"); 
+        digits.put(7, "sieben"); 
+        digits.put(8, "acht"); 
+        digits.put(9, "neun"); 
+        digits.put(10, "zehn"); 
+        digits.put(11, "elf"); 
+        digits.put(12, "zwölf");
+        digits.put(13, "dreizehn"); 
+        digits.put(14, "vierzehn"); 
+        digits.put(15, "fünfzehn");
+        digits.put(16, "sechzehn"); 
+        digits.put(17, "siebzehn"); 
+        digits.put(18, "achtzehn");
+        digits.put(19, "neunzehn"); 
 
-    private final static String[] einerAnom = {"null","eins"}; // Inconsistent wording for single digits
-    private final static String[] zehnerAnom = {"elf","zwoelf"}; // Inconsistent wording for decimals
-    private final static String[] einer = {"", "ein", "zwei", "drei", "vier", "fuenf", "sechs", "sieben", "acht", "neun"}; // Wording for single digits
-    private final static String[] zehner = {"", "zehn", "zwanzig", "dreissig", "vierzig", "fuenfzig", "sechzig", "siebzig", "achtzig", "neunzig"}; // Wording for decimals
-    
-    public static String getZahlWort(int x) {
-        StringBuilder ret = new StringBuilder();
-        String input = String.valueOf(x);
-        final int sLen = input.length();
-        
-        if (input.length() > 1) {
-            char[] tmp = input.toCharArray();
-            char tmpC = tmp[sLen-1];
-            tmp[sLen-1] = tmp[sLen-2];
-            tmp[sLen-2] = tmpC;
-            StringBuilder sb = new StringBuilder();
-            for (char c : tmp) {
-                sb.append(c);
-            }
-            input = sb.toString();
+
+        digits2=new HashMap<>();
+        digits2.put(2,"zwanzig");
+        digits2.put(3,"dreißig"); 
+        digits2.put(4,"vierzig"); 
+        digits2.put(5,"fünfzig"); 
+        digits2.put(6,"sechszig"); 
+        digits2.put(7,"siebzig"); 
+        digits2.put(8,"achtzig"); 
+        digits2.put(9,"neunzig"); 
+    } 
+
+
+   public static String getZahlwort(int x) { 
+
+        String input = String.valueOf(x); 
+        String ret = "";    
+        if (input.length() == 1)
+        {
+            ret += digits.get(Integer.valueOf(input.substring(0,1)));
         }
-        
-        List<Character> chars = input.chars()
-        .mapToObj(e -> (char)e)
-        .collect(Collectors.toList());
-        
-        Stream<String[]> characterStream = IntStream.range(0,chars.size()).mapToObj(lIndex -> {
-            int intRepr = Integer.valueOf(String.valueOf(chars.get(lIndex)));
-
-            int c = lIndex;
-            String[] retArr = new String[2];
-            System.out.println(c);
-            System.out.println(sLen-2);
-            System.out.println(intRepr);
-            if ((c == sLen-2) || (sLen == 1)) {
-                if (intRepr == 1){
-                    retArr[0] = einerAnom[1];
-                } else if (intRepr == 0) {
-                    retArr[0] = einerAnom[0];
-                } else {
-                    retArr[0] = einer[intRepr];
-                }
-            } else if ((c == sLen-1) && sLen > 1) {
-                if (intRepr < 2) {
-                    if (chars.get(c-1) == (char) 1 || chars.get(c-1) == (char) 2) {
-                        if (Integer.valueOf(String.valueOf(chars.get(c-1))) == 1) {
-                            retArr[0] = zehnerAnom[0];
-                        } else if (Integer.valueOf(String.valueOf(chars.get(c-1))) == 2) {
-                            retArr[0] = zehnerAnom[1];
-                        } 
-                    } else {
-                        retArr[0] = zehner[0];
-                    }
-                } else {
-                    retArr[0] = zehner[intRepr];
-                }
-            } else if (c == sLen-3) {
-                if (intRepr != 0) {
-                    retArr[0] = einer[intRepr]+"hundert";
-                } else {
-                    retArr[0] = "";
-                }
-            } else if (c == sLen-4) {
-                if (intRepr != 0) {
-                    retArr[0] = einer[intRepr]+"tausend";
-                } else {
-                    retArr[0] = "";
-                }
-
-        characterStream.forEach(s -> {
-            if (Integer.valueOf(s[1]) == sLen-2) {                
-                if (!((chars.get(sLen-2) == (char) 1 || chars.get(sLen-2) == (char) 2) && (chars.get(sLen-1) == (char) 1))) {
-                    ret.append(s[0]);
-                    ret.append("und");
-                }
-            } else {
-                ret.append(s[0]);
+            
+        else if (input.length() >= 2)
+        {
+            if (input.length() == 3)
+            {
+                ret += digits.get(Integer.valueOf(input.substring(0,1)))+"hundert";
             }
-        });
+            if (Integer.valueOf(input.substring(input.length()-2,input.length())).intValue()<20)
+                ret += digits.get(Integer.valueOf(input.substring(input.length()-2,input.length())));
+            else
+                ret += digits.get(Integer.valueOf(input.substring(input.length()-1,input.length()))) + "und" +digits2.get(Integer.valueOf(input.substring(input.length()-2,input.length()-1)));
+        }
+        return ret;
 
+   } 
 
-        return ret.toString();
-    }
-    public static void main(String[] args) {
-        System.out.println(getZahlWort(24));
-        System.out.println(getZahlWort(124));
-        System.out.println(getZahlWort(2));
-        System.out.println(getZahlWort(1));
-        System.out.println(getZahlWort(12));      
-    }
+   public static Stream<String> getZahlStream(int start, int stop){
+       return IntStream.rangeClosed(start, stop).mapToObj(s -> getZahlwort(s));
+   }
 }
