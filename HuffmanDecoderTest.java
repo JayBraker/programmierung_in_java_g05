70017ab1ede538889c9cfd2f123db26b1cdff68d
Josha Bartsch <jaybraker1@gmail.com>
2022-01-12 16:05:09 +0100

    Generalized locally used testpath

--- HuffmanDecoderTest.java
+++ HuffmanDecoderTest.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class HuffmanDecoderTest {
   public static void main(String[] args) {
     String decodedWord =
-        HuffmanDecoder.decode(new File("C:\\Users\\og415626\\Documents\\HuffmanCode.txt"));
+        HuffmanDecoder.decode(new File("HuffmanCode.txt"));
     System.out.println(decodedWord);
   }
 }
